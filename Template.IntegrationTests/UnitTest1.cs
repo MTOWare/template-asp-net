d3f5e7b4b2222f376d1df82aa54f793409038cee
Carlos Montalvo <carlosmontalvo@zetanova.com>
2021-04-07 02:16:53 -0500

    Add Template

---
+++ Template.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Template.IntegrationTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
