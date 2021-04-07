405c9e5645a7860dbc47a3e2138a784a23174d39
Carlos Montalvo <carlosmontalvo@zetanova.com>
2021-04-07 03:41:56 -0500

    Add Connection String

--- Template.Api/Startup.cs
+++ Template.Api/Startup.cs
@@ -1,16 +1,11 @@
+using Template.Infraestructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Template.Api
 {
@@ -28,6 +23,11 @@ namespace Template.Api
         {
 
             services.AddControllers();
+
+            services.AddDbContext<{{Database_Name}}Context>(options =>
+               options.UseNpgsql(Configuration.GetConnectionString("{{Database_Name}}Connection"))
+            );
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Template.Api", Version = "v1" });
